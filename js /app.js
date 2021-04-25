64f56ee0a570734499612672f099c4db5e221ab1
abu-nofal <anofal719@gmail.com>
2021-04-25 13:49:56 +0300

    add a class lap 1

---
+++ js /app.js
@@ -0,0 +1,14 @@
+let userName =prompt('welcome and hello, \n please enter your lovely name   ');
+console.log(userName);
+let age =prompt('please enter your age ');
+console.log(age);
+let Live=prompt('where are you live ');
+console.log(Live);
+let like=prompt('Mention something you like about yourself ');
+console.log(like)
+
+
+alert('welcom ' + userName + ' you are live in ' + Live + 
+' thats a wonderfull place actually, your  '
++ like + ' its a good thing great job to maention that ' 
++ ' you have ' + age + ' year oh mashallah i wish to you a great life ');
